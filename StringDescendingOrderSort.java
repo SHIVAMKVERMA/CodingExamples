28c7bb971b1fdfa84a4115484e0af5c4f9fe0fe5
SHIVAM VERMA <SHIVAMKVERMA@users.noreply.github.com>
2019-06-27 08:53:55 +0530

    Added StringDescendentOrderSort.java
    Print the second name from string separated via comma in a string array with descending order
    Added File: StringDescendeningOrderSort
    Author: Shivam Verma

---
+++ StringDescendingOrderSort.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class StringDescendingOrderSort {
+	public static void main(String[] args) {
+		String[] str = {"Shivam,Verma", "Arjun,Joshi", "Saurabh,Suman","Ojaswita,Mishra"};	
+
+		printSplitAndReverseStringArray(str);
+	}
+
+	public static void printSplitAndReverseStringArray(String[] array) {
+		int arrLength = array.length;
+
+		List<String> stringList = new ArrayList<String>();
+
+		String[] str3 = new String[arrLength];
+
+		for (String str: array) {
+			stringList.add(str);
+		}
+
+		for (int i = 0; i < stringList.size(); i++) {
+		
+			String str1 = stringList.get(i);
+			
+			String[] str2 = str1.split(",");
+			str3[i] = str2[str2.length - 1];
+		}
+
+		Arrays.sort(str3, Collections.reverseOrder());
+
+		for (String sortedStr : str3) {
+			System.out.println(sortedStr);
+		}
+	}
+}
